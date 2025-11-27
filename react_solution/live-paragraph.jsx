976bf4f69350f723cfdfbd71ba5abf2ae4f9f4a4
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-11-27 11:27:23 +0530

    react done

--- react_solution/live-paragraph.jsx
+++ react_solution/live-paragraph.jsx
@@ -1,5 +1,20 @@
-import {  } from 'react';
+import {} from 'react';
 import { createRoot } from 'react-dom/client';
 
+function LiveText() {
+  const [text, setText] = useState('');
+
+  const onChange = ({ target: { value } }) => {
+    setText(value);
+  };
+
+  return (
+    <>
+      <input type="text" onChange={onChange} value={text} />
+      <p>{text}</p>
+    </>
+  );
+}
+
 const root = createRoot(document.getElementById('root'));
-root.render();
+root.render(<LiveText />);
