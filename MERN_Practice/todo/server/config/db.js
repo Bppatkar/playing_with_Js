164c04962a7b0184f2e2d2eaf537ef8e1dadc8e8
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-11-27 22:01:37 +0530

    saving some data

--- MERN_Practice/todo/server/config/db.js
+++ MERN_Practice/todo/server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 async function connectDB() {
   try {
     await mongoose.connect(
-      'mongodb+srv://bhanupratappatkar777:mernPracticeInterview@cluster0.22hxd2o.mongodb.net'
+      process.env.MONGOURI
     );
     console.log('Database Connected Successfully');
   } catch (error) {
