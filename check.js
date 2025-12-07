c437ca2ee07dd6e69d85bb46ddf613c8639b276a
Bhanu Pratap Patkar <bhanupratappatkar777@gmail.com>
2025-12-07 22:11:11 +0530

    throttle debouncing add

--- check.js
+++ check.js
@@ -680,3 +680,40 @@ bar();
 //   });
 // });
 
+//! debouncing
+// const input = document.getElementById('inputData');
+
+// const debounce = (func, delay) => {
+//   let timer;
+//   return function (...args) {
+//     clearTimeout(timer);
+//     timer = setTimeout(() => {
+//       func(...args);
+//     }, delay);
+//   };
+// };
+
+// const myFunc = function (e) {
+//   const query = e.target.value;
+//   console.log(query);
+
+//   // fetch(`http://api.example.com/search?query=${query}`)
+//   //   .then((res) => res.json())
+//   //   .then((result) => console.log(result));
+// };
+
+// input.addEventListener('input', debounce(myFunc, 500));
+
+//! throttling
+
+// const throttle = (func, delay) => {
+//   let shouldCall = true;
+//   return function (...args) {
+//     if (!shouldCall) return;
+//     shouldCall = false;
+//     setTimeout(() => {
+//       func(...args);
+//       shouldCall = true;
+//     }, delay);
+//   };
+// };
